b568bd6adc63ffebb8938d22db7dce9fe550d05a
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-06-16 23:25:23 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import express, {
 // import { Pool } from "pg";
 
 import { initDB, pool } from "./db";
+import { userRoute } from "./modules/user/user.route";
 
 const app: Application = express();
 // const port = config.port;
@@ -22,31 +23,7 @@ app.get("/", (req: Request, res: Response) => {
   //   res.send('Hello World!')
   res.status(200).json({ message: "Express Server", authoor: "Next level" });
 });
-
-app.post("/api/users", async (req: Request, res: Response) => {
-  // console.log(req.body)
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(
-      `
-    INSERT INTO users(name,email,password,age)  VALUES($1,$2,$3,$4) RETURNING *  
-    `,
-      [name, email, password, age],
-    );
-    // console.log(result);
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      error: error,
-    });
-  }
-});
+app.use('/api/users',userRoute)
 
 app.get("/api/users", async (req: Request, res: Response) => {
   try {
